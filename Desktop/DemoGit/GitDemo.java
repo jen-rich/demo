43815c95333c437d81005a28330787857538a684
Ethan Falk <FalkER19@pwcs-edu.org>
2019-01-08 13:01:36 -0500

    Fixed the code

--- Desktop/DemoGit/GitDemo.java
+++ Desktop/DemoGit/GitDemo.java
@@ -1,10 +1,10 @@
+package demoGit;
 import java.util.*;
 
 public class GitDemo {
     public static String changer(String name) {
         StringBuilder sb = new StringBuilder();
-        sb.append(name);
-        for(int i = name.length(); i > 0; i++) {
+        for(int i = name.length() - 1; i >= 0; i--) {
             sb.append(name.charAt(i));
         }
         return sb.toString();
@@ -12,6 +12,6 @@ public class GitDemo {
     
     public static void main(String[] args) {
         Scanner scan =  new Scanner(System.in);
-        changer(scan.nextLine());
+        System.out.print(changer(scan.nextLine()));
     }
 }
